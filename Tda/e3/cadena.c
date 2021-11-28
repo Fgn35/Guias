9c67380f909ab51b30990f087e1afdcc0a355047
Fgn35 <francogaytan35@gmail.com>
2021-11-28 16:57:38 -0300

    e3 tda

--- Tda/e3/cadena.c
+++ Tda/e3/cadena.c
@@ -8,9 +8,9 @@ struct cadena{
 };
 
 
-cadena_t * cadena_crear(const char *s){
+cadena_t *cadena_crear(const char *s){
     int len = strlen(s);
-    cadena_t *str = malloc (sizeof(cadena_t)+ len + 1);
+    cadena_t *str = malloc (sizeof(cadena_t) + len);
     if (str == NULL) return NULL;
     str->str = s;
     str->largo = len;
@@ -23,10 +23,36 @@ void cadena_destruir(cadena_t *s){
     free(s);
 }
 
-cadena_t * cadena_copiar(cadena_t *s1, const cadena_t *s2){
-    
+cadena_t *cadena_copiar(cadena_t *s1, const cadena_t *s2){
+    if(s1->largo < s2->largo){
+        cadena_t *aux = realloc(s1, sizeof(cadena_t) + s2->largo);
+        if (aux == NULL) return NULL;
+        
+        s1 = aux;
+        char str[s2->largo];
+        strcpy(str, s2);
+        strcpy(s1->str, str);
+        s1->largo = s2->largo;
+        return s1;
+
+    }
+    strcpy(s1->str, s2->str);
+    s1->largo = strlen(s2);
+    return s1;
 }
-cadena_t * cadena_clonar(const cadena_t *);
-cadena_t * cadena_concatenar(cadena_t *, const cadena_t *);
-cadena_t * cadena_strcpy(cadena_t *, const char *);
-cadena_t * cadena_strcat(cadena_t *, const char *);
+
+cadena_t *cadena_clonar(const cadena_t *s){
+    cadena_t *clon = malloc(sizeof(s));
+    strcpy(clon->str, s->str);
+    clon->largo = s->largo;
+    return clon; 
+}
+cadena_t *cadena_concatenar(cadena_t *s1, const cadena_t *s2){
+    cadena_t *str = malloc(sizeof(cadena_t) + s1->largo + s2->largo);
+    strcat(str, s1);
+    strcat(str, s2);
+    str->largo = s1->largo + s2->largo;
+    return str;
+}
+cadena_t *cadena_strcpy(cadena_t *s1, const char *s2);
+cadena_t *cadena_strcat(cadena_t *s1, const char *s2);
